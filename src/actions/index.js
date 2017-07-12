54b57e0767e61861552a09a0a99ea07579c1ffb1
ASSEENONSCREEN\tamas.jonas <tamas.jonas@asos.com>
2017-07-12 17:19:45 +0100

    new

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-export const GET_MOVIE_TMDB = 'GET_MOVIE_TMDB';
 export const GET_CONFIG = 'GET_CONFIG';
+export const GET_MOVIE_TMDB = 'GET_MOVIE_TMDB';
+export const GET_MOVIE_CREW = 'GET_MOVIE_CREW';
 
 export function getConfig(){
   const url = `https://api.themoviedb.org/3/configuration?api_key=?&api_key=cfe422613b250f702980a3bbf9e90716`
@@ -29,6 +30,21 @@ export function getMovie(movieId){
       })
       .catch((err) => {
         dispatch({type:"GET_MOVIE_TMDB_REJECTED", movie: err})
+      })
+  }
+}
+
+export function getCrew(movieId){
+  const url = `https://api.themoviedb.org/3/movie/${movieId}/credits?&api_key=cfe422613b250f702980a3bbf9e90716`
+
+  return function(dispatch) {
+    dispatch({type: "GET_MOVIE_CREW"});
+    axios.get(url)
+      .then((response) => {
+        dispatch({type:"GET_MOVIE_CREW_FULFILLED",crew: response.data.cast})
+      })
+      .catch((err) => {
+        dispatch({type:"GET_MOVIE_CREW_REJECTED",crew: err})
       })
   }
 }
--- src/components/MovieProfileContainer.js
+++ src/components/MovieProfileContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Style from 'style-it';
 import { connect } from 'react-redux';
 
 import { Grid, Col, Row } from 'react-bootstrap';
@@ -10,60 +11,75 @@ class MovieProfileContainer extends Component {
     const img = this.props.config.config.images;
     let image = <img src={img.base_url + img.poster_sizes[3] + movie.poster_path} className="img-responsive center-block" alt="poster" />;
     let genres = movie.genres.map(genre => (<li key={genre.id}>{genre.name}</li>));
+    let crew = this.props.movie.crew.slice(0,9)
+          .map((cr,i) => (
+            <li key={i} className="col-sm-4">
+              <p className="crew-col text-center">
+                <strong>{cr.name}</strong><br />
+                <small>{cr.character}</small>
+              </p>
+            </li>
+          ));
 
     return(
-      <Grid className="grid-inverse">
-        <Row className="show-grid">
-          <Col xs={6}>
-            {image}
-          </Col>
-          <Col xs={6}>
-            <Row>
-              <Col xs={12}>
-                <h1>{movie.original_title}</h1>
-              </Col>
-            </Row>
-            <Row>
-              <hr />
-            </Row>
-            <Row className="show-grid">
-              <Col xs={4}>
-                <h4>Release date</h4>
-                {movie.release_date}
-              </Col>
-              <Col xs={4}>
-                <h4>Runtime</h4>
-                {movie.runtime} min
-              </Col>
-              <Col xs={4}>
-                <h4>Popularity</h4>
-                {movie.popularity}
-              </Col>
-            </Row>
-            <Row>
-              <hr />
-            </Row>
-            <Row className="show-grid">
-              <Col xs={12}>
-                <h4>Overview</h4>
-                {movie.overview}
-              </Col>
-            </Row>
-            <Row>
-            <hr />
-            </Row>
-            <Row>
-              <Col xs={12}>
-                <ul className="list-inline">
-                  {genres}
-                </ul>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
+      <div>
+        <Style>
+          {`
+            .main-header:before {
+              background-image: url(${img.base_url + img.backdrop_sizes[1] + movie.backdrop_path});
+            }
+          `}
+        </Style>
 
+        <Grid className="main-header" fluid={true}>
+          <div className="custom_bg">
 
-      </Grid>
+            <Col sm={6} smOffset={3} className="main-col">
+
+              <Row className="show-grid">
+                <Col xs={6} className="poster-col">{image}</Col>
+
+                <Col xs={6}>
+
+                  <Row className="profile-row">
+                    <Col xs={12}><h1>{movie.original_title}</h1><h4>{movie.tagline}</h4></Col>
+                  </Row>
+
+                  <Row className="profile-row">
+                    <Col xs={4}><h4>Release date</h4>{movie.release_date}</Col>
+                    <Col xs={4}><h4>Runtime</h4>{movie.runtime} min</Col>
+                    <Col xs={4}><h4>Vote Rate</h4>{movie.vote_average} / 10</Col>
+                  </Row>
+
+                  <Row className="profile-row">
+                    <Col xs={12}><h4>Overview</h4>{movie.overview}</Col>
+                  </Row>
+
+                  <Row className="profile-row">
+                    <Col xs={12}>
+                      <ul className="list-inline">
+                        {genres}
+                      </ul>
+                    </Col>
+                  </Row>
+
+                  <Row className="profile-row">
+                    <Col xs={12}>
+                      <ul className="list-inline">
+                        {crew}
+                      </ul>
+                    </Col>
+                  </Row>
+
+                </Col>
+
+              </Row>
+
+            </Col>
+          </div>
+
+        </Grid>
+    </div>
     )
   }
 }
