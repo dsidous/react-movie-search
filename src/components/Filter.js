6878e739a612cb15b83f59221947b27bd1b11a17
dsidous <jonastamas2@gmail.com>
2017-07-20 17:31:13 +0100

    new

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,32 +1,53 @@
 import React, { Component} from 'react'
-import { AsyncTypeahead } from 'react-bootstrap-typeahead';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+import Select from 'react-select';
 
-import { Navbar, FormGroup, FormControl } from 'react-bootstrap';
+import { Navbar, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import 'react-select/dist/react-select.css';
 
 class Filter extends Component {
 
-  componentDidMount(){
-    this.props.dispatch(actions.getConfig());
+  constructor(){
+    super();
+
     this.state = {
       page : 1,
-      year : 2017
+      primary_release_year : 2017
     }
 
-    this.props.dispatch(actions.getDiscoverMovies('&page=1&year=2017'));
-
     this.handleChange = this.handleChange.bind(this);
+
   }
 
-  handleChange(e){
-    const option = e.target.value;
-    console.log(option);
-    this.props.dispatch(actions.getDiscoverMovies('&page=1&primary_release_year=' + option));
+  componentDidMount(){
+    this.props.dispatch(actions.getConfig());
+    this.props.dispatch(actions.getGenres());
+
+    this.props.dispatch(actions.getDiscoverMovies('&page=1&primary_release_year=2017'));
+  }
+
+  objectToQueryStr(paramsObj) {
+    return Object
+          .keys(paramsObj)
+          .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(paramsObj[key])}`)
+          .join('&');
+  }
+
+  handleChange(val){
+    console.log("Selected: " + JSON.stringify(val))
+    // this.setState({ primary_release_year: option }, function() {
+    //   const query = this.objectToQueryStr(this.state);
+    //   this.props.dispatch(actions.getDiscoverMovies('&' + query));
+    // });
   }
 
   render(){
-    let year_options = Array(117).fill().map((_, i) => <option value={2016 - i}>{2016 - i}</option>);
+    let year_options = Array(118).fill().map((_, i) => <option key={i} value={2017 - i}>{2017 - i}</option>);
+    let genres = (typeof this.props.config.genres[0] !== 'undefined') ? this.props.config.genres : [];
+    //let genres_options = genres.map( genre => <option key={genre.id} value={genre.id}>{genre.name}</option> );
+    let genres_options = [{value: 1, label: 'action'},{value: 2, label: 'comedy'}]
+
 
     return(
       <div>
@@ -40,12 +61,25 @@ class Filter extends Component {
           <Navbar.Collapse>
             <Navbar.Form pullLeft>
               <FormGroup bsSize="small">
-                <FormControl componentClass="select" placeholder="select" onChange={this.handleChange}>
-                  <option value="0">None</option>
-                  <option value="2017" selected>2017</option>
+                <FormControl componentClass="select" placeholder="select" onChange={this.handleChange} defaultValue="2017">
+                  <option value="">None</option>
                   {year_options}
                 </FormControl>
               </FormGroup>
+              <Select
+                name="select_genres"                
+                simpleValue={true}
+                options={genres_options}
+                onChange={this.handleChange}
+                multi={true}
+              />
+              {/* <FormGroup controlId="formControlsSelectMultiple">
+                <ControlLabel>Select Genres</ControlLabel>
+                <FormControl componentClass="select" multiple ref="select_genres" onChange={this.handleChange}>
+                  <option value="">None</option>
+                  {genres_options}
+                </FormControl>
+              </FormGroup> */}
             </Navbar.Form>
           </Navbar.Collapse>
         </Navbar>
--- src/reducers/config.js
+++ src/reducers/config.js
@@ -1,9 +1,13 @@
-const config = (state = {config:{}}, action) => {
+const config = (state = {config:{}, genres:{}}, action) => {
 
   switch (action.type) {
     case 'GET_CONFIG_FULFILLED':
       return Object.assign({}, state, {
         config: action.config
+      })
+    case 'GET_GENRES_TMDB_FULFILLED':
+      return Object.assign({}, state, {
+        genres: action.genres
       })
     default:
       return state;
