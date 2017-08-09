f59df8d965975d5d066c7d03b32e7ac49b2c9d0c
ASSEENONSCREEN\tamas.jonas <tamas.jonas@asos.com>
2017-08-09 17:20:52 +0100

    tidy

--- src/containers/MovieProfileContainer.js
+++ src/containers/MovieProfileContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import ReactDom from 'react-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import * as Vibrant from 'node-vibrant'
+// import * as Vibrant from 'node-vibrant'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 import MovieProfile from '../components/MovieProfile'
@@ -10,9 +10,9 @@ import * as actions from '../actions'
 
 class MovieProfileContainer extends Component {
 
-  state = {
-    dcolor : []
-  }
+  // state = {
+  //   dcolor : []
+  // }
 
   static propTypes = {
     config: PropTypes.object.isRequired,
@@ -23,28 +23,28 @@ class MovieProfileContainer extends Component {
     router: PropTypes.object.isRequired
   }
 
-  componentDidMount() {
-    this.getPalette();
-  }
+  // componentDidMount() {
+  //   this.getPalette();
+  // }
 
-  componentWillReceiveProps() {
-    this.getPalette();
-  }
+  // componentWillReceiveProps() {
+  //   this.getPalette();
+  // }
 
   componentDidUpdate = () => {
     ReactDom.findDOMNode(this).scrollIntoView();
   }
 
-  getPalette = (path) => {
-    if (this.props.movie.movie.poster_path) {
-      let path = this.props.config.config.images.base_url + this.props.config.config.images.poster_sizes[3] + this.props.movie.movie.poster_path;
-      Vibrant
-        .from(path)
-        .getSwatches((err,palette) => (this.setState({dcolor: palette.DarkVibrant._rgb})))
-    } else {
-      this.setState({dcolor: [0,0,0]})
-    }
-  }
+  // getPalette = (path) => {
+  //   if (this.props.movie.movie.poster_path) {
+  //     let path = this.props.config.config.images.base_url + this.props.config.config.images.poster_sizes[3] + this.props.movie.movie.poster_path;
+  //     Vibrant
+  //       .from(path)
+  //       .getSwatches((err,palette) => (this.setState({dcolor: palette.DarkVibrant._rgb})))
+  //   } else {
+  //     this.setState({dcolor: [0,0,0]})
+  //   }
+  // }
 
   handleMovieClick = (movieId) => {
     this.props.dispatch(actions.updateMovie(movieId));
@@ -77,7 +77,7 @@ class MovieProfileContainer extends Component {
                 crew={this.props.movie.crew}
                 videos={this.props.movie.videos}
                 images={this.props.movie.images}
-                dcolor={this.state.dcolor}
+                // dcolor={this.state.dcolor}
                 handleMovieClick={this.handleMovieClick}
                 handlePersonClick={this.handlePersonClick}
             />
