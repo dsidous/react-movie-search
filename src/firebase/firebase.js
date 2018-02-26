97b794f7592cb855427530b5f00c258e41447416
tamas jonas <jonastamas2@gmail.com>
2018-02-26 17:45:51 +0000

    firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,6 @@
 import * as firebase from "firebase";
-
-const config = {
-  apiKey: "AIzaSyAwjfdzWFoOsHHX2VWKWBQmszurb7NifIk",
-  authDomain: "movie-search-4e239.firebaseapp.com",
-  databaseURL: "https://movie-search-4e239.firebaseio.com",
-  projectId: "movie-search-4e239",
-  storageBucket: "movie-search-4e239.appspot.com",
-  messagingSenderId: "679577561174"
-};
-
+import config from './config';
+ 
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
 }
