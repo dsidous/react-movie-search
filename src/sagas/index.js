c6502b8b08e2d152f0ef906b0ad6a730fbc77934
dsidous <jonastamas2@gmail.com>
2018-09-10 10:10:51 +0100

    more sagas

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,15 @@
 import { all } from "redux-saga/effects";
 import { getConfigWatcher, getGenresWatcher } from "./configs";
 import { getDiscoverMoviesWatcher } from "./movies";
+import { getTopPeopleWatcher } from "./persons";
+import { getDiscoverMoviesWatcher } from "./tvs";
 
 export default function* rootSaga() {
-  yield all([getConfigWatcher, getGenresWatcher, getDiscoverMoviesWatcher]);
+  yield all([
+    getConfigWatcher,
+    getGenresWatcher,
+    getDiscoverMoviesWatcher,
+    getTopPeopleWatcher,
+    getDiscoverMoviesWatcher
+  ]);
 }
