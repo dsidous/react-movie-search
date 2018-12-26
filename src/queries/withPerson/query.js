7823ed639696fa1593ee5764f4bfdef2675c3e67
dsidous <jonastamas2@gmail.com>
2018-12-26 21:03:06 +0000

    person page

---
+++ src/queries/withPerson/query.js
@@ -0,0 +1,25 @@
+import gql from 'graphql-tag';
+
+export const query = gql`
+  query getPerson($personId: ID!) {
+    person(id: $personId) {
+      id
+      biography
+      birthday
+      deathday
+      name
+      place_of_birth
+      profile_path
+      movie_credits {
+        cast {
+          id
+          original_title
+          character
+          release_date
+          poster_path
+          vote_count
+        }
+      }
+    }  
+  }
+`;
