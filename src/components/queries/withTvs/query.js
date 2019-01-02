d9e501c47c725891a813e0c5f21cca36c7f064c0
dsidous <jonastamas2@gmail.com>
2019-01-02 06:10:20 +0000

    added tvs page

---
+++ src/components/queries/withTvs/query.js
@@ -0,0 +1,16 @@
+import gql from 'graphql-tag';
+
+export const query = gql`
+  query getTvs($query: String!) {
+    tvs(query: $query) {
+      id
+      backdrop_path
+      poster_path
+      name
+      overview
+      first_air_date
+      vote_average
+      genre_ids
+    }
+  }
+`;
---
+++ src/components/templates/Tvs/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import Filter from '../../organisms/Filter';
+import Result from "../../molecules/Result";
+
+class Tvs extends Component {
+
+  static propTypes = {
+    config: PropTypes.object.isRequired,
+    genres: PropTypes.array.isRequired,
+    movies: PropTypes.array.isRequired
+  };
+
+  static contextTypes = {
+    router: PropTypes.object.isRequired
+  }
+
+  componentDidUpdate = () => {
+    window.scrollTo(0, 0);
+  };
+
+  queryUpdate = (query) => {
+    this.context.router.history.push(`/tvs?${query}`);
+  }
+
+  render() {
+    if (this.props.loading || this.props.configLoading || this.props.genresLoading) {
+      return (
+        <div className="loader">
+          <i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+          <span className="sr-only">Loading...</span>
+        </div>
+      )
+    };
+
+    return (
+      <Filter query={this.props.query} genres={this.props.genres} queryUpdate={this.queryUpdate}>
+        <Result {...this.props} />
+      </Filter>
+    );
+  }
+}
+
+export default Tvs;
